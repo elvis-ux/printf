5540a52e79d35a0496366d96e8121f33f8865103
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-17 15:57:34 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: string
+ * Return: no of chars print
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, length = 0, ibuf = 0;
+	va_list arg;
+	int (*function)(va_list, char *, unsigned int);
+	char *size;
+
+	va_start(arg, format), size = malloc(sizeof(char) * 1024);
+	if (!format || !size || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if ([i + 1] == '\0')
+			{
+				print_buff(size, ibuf), free(size), va_end(arg);
+				return (-1);
+			}
+			else
+			{
+				function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handle_buf(butter, format[i], ibuf), length++, i--;
+				} else
+				{
+					length += function(arg, size, ibuf);
+					i += print_func(format, i + 1);
+				}
+			} i++;
+		} else
+			handle_buf(size, format[i], ibuf), length++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buff(size, ibuf), free(size), va_end(arg);
+	return (length);
+}
---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct print - printer functions
+ * @type_arg: identifier
+ * @func: function pointer
+ */
+
+typedef struct print
+{
+	char *type_arg;
+	int (*func)(va_list, char *,  unsigned int);
+} print_t;
+
+int _printf(const char *, format, ...);
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
+int print_func(const char *s, int index);
+unsigned int handle_buf(char *buf, char c, unsigned int ibuf);
+int print_buff(char *buf, unisigned int nbuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+
+#endif
