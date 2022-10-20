2883e6c0cf44bd50a1d6ce568acc6b5761fa73fa
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-19 20:18:10 -0700

    completed

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
 
 int _printf(const char * const format, ...)
 {
-	print_t m[] = {
+	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
 		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
